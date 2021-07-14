c8cf6e752dc6ff177ca1c1c9ca9099fab8aed91e
askjag07 <askjag07@gmail.com>
2021-07-14 20:19:19 +0530

    Updated dependence on .env file.

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -38,13 +38,9 @@ export default class NotFoundPage extends React.Component {
           </span>{" "}
           we couldn’t find what you were looking for.
           <br />
-          {process.env["NODE_ENV"] === "development" ? (
-            <>
-              <br />
-              Try creating a page in <code style={codeStyles}>src/pages/</code>.
-              <br />
-            </>
-          ) : null}
+          <br />
+          Try creating a page in <code style={codeStyles}>src/pages/</code>.
+          <br />
           <br />
           <Link to="/">Go home</Link>.
         </p>
