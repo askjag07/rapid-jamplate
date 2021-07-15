44650cde7c2f066b32f99570ac399d22d4b37551
Akshaj Jagarapu <67054376+askjag07@users.noreply.github.com>
2021-07-15 22:23:04 +0530

    Update layout.tsx

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -12,9 +12,9 @@ export default class Layout extends React.Component<{}, {}> {
           title={((s: string): string =>
             [""].includes(s)
               ? !!s
-                ? `${s[0]?.toUpperCase()}${s.slice(1)} | Rapid-JAMPlate`
-                : "Rapid-JAMPlate | askjag07"
-              : "Not Found | Rapid-JAMPlate")(
+                ? `${s[0]?.toUpperCase()}${s.slice(1)} | Rapid JAMPlate`
+                : "Rapid JAMPlate | askjag07"
+              : "Not Found | Rapid JAMPlate")(
             window.location.pathname.replace(/\//g, "")
           )}
           defer={false}
@@ -25,10 +25,10 @@ export default class Layout extends React.Component<{}, {}> {
             name="viewport"
             content="width=device-width, initial-scale=1.0"
           />
-          <meta name="description" content="Rapid-JAMPlate" />
+          <meta name="description" content="Rapid JAMPlate" />
           <meta
             name="keywords"
-            content="askjag07, akshaj jagarapu, rapid-jamplate, rapid jamplate, gatsby starter, rapid gatsby starter, jamplate gatsby starter"
+            content="askjag07, akshaj jagarapu, rapid jamplate, gatsby starter, rapid gatsby starter, jamplate gatsby starter"
           />
           <meta name="author" content="Akshaj Jagarapu" />
           <meta name="image" content="https://yourdomain.tld/i.png" />
@@ -38,12 +38,12 @@ export default class Layout extends React.Component<{}, {}> {
           "@context": "https://schema.org",
           "@type": "Organization",
           "@id": "https://yourdomain.tld",
-          "name": "Rapid-JAMPlate",
+          "name": "Rapid JAMPlate",
           "url": "https://yourdomain.tld",
           "logo":
             "https://yourdomain.tld/i.png",
           "telephone": "+11234567890",
-          "description": "Rapid-JAMPlate",
+          "description": "Rapid JAMPlate",
           "speakable": {
             "@type": "SpeakableSpecification",
             "xpath": [
